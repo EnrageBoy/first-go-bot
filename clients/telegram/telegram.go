70474d88a3825648c63aea63d2183d0a9250a0f6
Eugene_R <enrageboy82@mail.ru>
2024-07-30 01:27:04 +0800

    Добавил Обработку ошибок для Клиент телеграм

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -2,14 +2,62 @@ package telegram
 
 import (
 	"net/http"
+	"net/url"
+	"path"
+	"strconv"
 )
 
 type Client struct {
 	host     string
 	basePath string
-	Client   http.Client
+	client   http.Client
 }
 
-func New(host string, token string) {
+func New(host string, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+
+}
+
+func (c *Client) Update(offset, int, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("offset", strconv.Itoa(limit))
+
+	//do request <- getUpdates
+
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.Wrap("can't do request", err) }()
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, e.Wrap(errMsg, err) // ошибка на 12:30
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, e.Wrap(errMsg, err)
+	}
+}
+
+func (c *Client) SendMessage() {
 
 }
